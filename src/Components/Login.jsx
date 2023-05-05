487d458dc754a7723e39337e07fe1d3161d77ee1
yash kotti <ykotti@mdsol.com>
2023-05-05 18:09:19 +0530

    Done changes Such as Adding tool name to the card and adjusting the App and its version inLine Etc.

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -11,7 +11,7 @@ import {
     MDBFooter,
     MDBNavbar,
     MDBNavbarBrand,
-    MDBSpinner
+    MDBSpinner,
 }
     from 'mdb-react-ui-kit';
 
@@ -29,7 +29,7 @@ const Login = () => {
 
     urlHeaderData.append('Content-Type', 'application/json');
     urlHeaderData.append('Accept', 'application/json');
-    
+
 
 
     useEffect(() => {
@@ -75,6 +75,7 @@ const Login = () => {
                 console.log(error.response);
                 if (error.response.status == 400) {
                     alert("Server responded : Wrong user Credentials");
+                    isLoading = false;
                 }
             } else if (error.request) {
                 alert("network error");
@@ -82,6 +83,7 @@ const Login = () => {
                 console.log(error);
                 alert("Error please see console ");
             }
+
         }
 
     }
@@ -89,9 +91,8 @@ const Login = () => {
 
 
         <>
-
-            <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
-                <MDBNavbar className=" " style={{ backgroundColor: 'rgba(51, 81, 119, 1)' }}>
+            <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh', margin: '0 auto' }}>
+                <MDBNavbar className=" " style={{ backgroundColor: 'rgba(51, 81, 119, 1)', margin: '0' }}>
                     <MDBContainer fluid>
                         <MDBNavbarBrand href='#'>
                             <img
@@ -102,24 +103,22 @@ const Login = () => {
                                 loading='lazy'
                             />
                         </MDBNavbarBrand>
-                        <h3 className="fw-bolder mx-auto my-auto text-white">TS - URL Deployment Tool</h3>
                     </MDBContainer>
                 </MDBNavbar>
 
-                <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', flex: 1 }}>
-                    <MDBContainer fluid className="d-flex align-items-center justify-content-center flex-grow-1">
-                        <MDBCard className='p-5 shadow-5' style={{ width: '40%', background: 'hsla(0, 0%, 100%, 0.8)', backdropFilter: 'blur(30px)', marginTop: '5%', marginBottom: '5%' }}>
+                <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', flex: 1, margin: '0' }}>
+                    <MDBContainer fluid className="d-flex align-items-center justify-content-center flex-grow-1" style={{ margin: '0' }}>
+                        <MDBCard className='p-5 shadow-5' style={{ width: '40%', background: 'hsla(0, 0%, 100%, 0.8)', backdropFilter: 'blur(30px)', margin: '0' }}>
+                            <h2 className="fw-bold text-center">TS - URL Deployment Tool</h2>
                             <MDBCardBody className='p-1 text-center'>
-                                <h4 className="fw-bold mb-5">Welcome</h4>
-                                <MDBInput wrapperClass='mb-4 mt-2' label='Lab Username' id='username' type='text'
+                                <h4 className="fw-bold mb-4 mt-2">Welcome</h4>
+                                <MDBInput wrapperClass=' mt-2' label='Lab Username' id='username' type='text'
                                     value={username}
                                     onChange={(event) => setUsername(event.target.value)}
-                                    placeholder="Username"
                                     required />
                                 <MDBInput wrapperClass='mb-4 mt-4' label='Password' id='password' type='password'
                                     value={password}
                                     onChange={(event) => setPassword(event.target.value)}
-                                    placeholder="Password"
                                     required
                                 />
                                 {isLoading ? (
@@ -134,8 +133,8 @@ const Login = () => {
                         </MDBCard>
                     </MDBContainer>
 
-                    <MDBFooter className="fs-6">
-                        <div className='text-center p-2' style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)', 'fontSize': '14px' }}>
+                    <MDBFooter style={{ margin: '0' }}>
+                        <div className='text-center p-2' style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)', 'fontSize': '12px' }}>
                             <a href="http://medidata.com">
                                 Medidata Solutions, Inc., a Dassault Systèmes company<br />
                             </a>
@@ -150,144 +149,6 @@ const Login = () => {
             </div>
 
         </>
-
-        // <div className="rootdiv">
-
-        //     <Navbar bg="dark" variant="dark">
-        //         <Container >
-        //             <Navbar.Brand href="/" >
-        //                 <img
-        //                     alt="Medidata logo"
-        //                     src="https://mdsol.github.io/medidata_design_system/static/media/Medidata_Logo_white.169a9612.png"
-        //                     className="logo"
-        //                 // className="d-inline-block align-top"
-        //                 />
-        //             </Navbar.Brand>
-        //             <h2 className="Titletext">TS - URL Deployment Tool</h2>
-        //             {/* <Navbar.Text className="TitleText">
-        //             TS - URL Deployment Tool
-        //             </Navbar.Text> */}
-        //         </Container>
-        //     </Navbar>
-        //     <br /><br /><br />
-
-        //     <Card className="card">
-        //         {/* <Card.Header as="h4">Login</Card.Header> */}
-        //         <h4>Login</h4>
-        //         <Card.Body>
-        //             <div className="rows">
-        //                 <div className="row">
-        //                     <img src="./images/4957136.jpg" alt="Login Photo" />
-        //                 </div>
-        //                 <div className="row">
-        //                     <form onSubmit={handleSubmit}>
-
-        //                         <FloatingLabel
-        //                             controlId="username"
-        //                             label="User name"
-        //                             className="mb-3"
-        //                         >
-        //                             <Form.Control type="text"
-        //                                 value={username}
-        //                                 onChange={(event) => setUsername(event.target.value)}
-        //                                 placeholder="Username"
-        //                                 required
-        //                             />
-        //                         </FloatingLabel>
-        //                         <br />
-        //                         <FloatingLabel controlId="password" label="Password">
-        //                             <Form.Control type="password"
-        //                                 value={password}
-        //                                 onChange={(event) => setPassword(event.target.value)}
-        //                                 placeholder="Password"
-        //                                 required />
-        //                         </FloatingLabel>
-        //                         <br /><br />
-        //                         <div className="d-grid gap-2">
-        //                             <Button variant="outline-primary" type="submit" size="lg">
-        //                                 Login
-        //                             </Button>
-        //                         </div>
-
-        //                     </form>
-        //                 </div>
-        //             </div>
-
-        //         </Card.Body>
-        //     </Card>
-        // </div >
-
-
-
-
-        /* <div className="App">
-            <header>
-                <nav>
-                    <div className="logo">Logo</div>
-                    <ul>
-                        <li><a href="/">Home</a></li>
-                        <li><a href="/about">About</a></li>
-                    </ul>
-                </nav>
-            </header>
-            <main>
-                <div className="login-card">
-                    <h2>Login</h2>
-
-                    <form onSubmit={handleSubmit}>
-                        <div>
-                            <input
-                                type="text"
-                                name="username"
-                                id="username"
-                                value={username}
-                                onChange={(event) => setUsername(event.target.value)}
-                                placeholder="Username"
-                                required
-                            />
-                        </div>
-                        <div>
-                            <input
-                                type="password"
-                                name="password"
-                                id="password"
-                                value={password}
-                                onChange={(event) => setPassword(event.target.value)}
-                                placeholder="Password"
-                                required
-                            />
-                        </div>
-                        <button type="submit">Submit</button>
-                    </form>
-
-                    {/* <form>
-            <div>
-              <label htmlFor="username">Username</label>
-              <input type="text" id="username" name="username" required />
-            </div>
-            <div>
-              <label htmlFor="password">Password</label>
-              <input type="password" id="password" name="password" required />
-            </div>
-            <button type="submit">Login</button>
-          </form> */
-        /* </div>
-    </main>
-    <footer>
-        <div className="company-info">
-            <h3>Company Info</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam commodo felis non augue luctus, a lobortis elit commodo.</p>
-        </div>
-        <div className="links">
-            <ul>
-                <li><a href="/contact">Contact Us</a></li>
-                <li><a href="/privacy">Privacy Policy</a></li>
-                <li><a href="/terms">Terms of Service</a></li>
-            </ul>
-        </div>
-    </footer>
-</div> */
-
     );
 }
 
